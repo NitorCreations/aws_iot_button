d7c1f8072dfc75ce763b3c59d2eec10f20e5a8e7
Mika Majakorpi <mika@majakorpi.net>
2017-07-31 14:52:04 +0300

    Move to project root and output app URL after deployment

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+cacert/gen_ca_cert.sh
+cacert/register_ca_certificate.sh
+cacert/gen_device_cert.sh
+cd button-service
+sls deploy
+./init_iot_logging.sh
+./setup_app_env.sh
+cd ../app
+npm run build
+APP_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | contains("apphostingbucket")) | .Name')
+aws s3 sync build s3://$APP_BUCKET
+
+cat << MARKER
+
+****************************************************************
+App available at $(aws cloudformation describe-stacks --stack-name button-service-dev | jq -r '.Stacks[] | .Outputs[] | select(.OutputKey == "AppURL") | .OutputValue')
+****************************************************************
+
+MARKER
