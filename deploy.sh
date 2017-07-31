c23d890793be408ffb34bb77ec455ee5660b9850
Mika Majakorpi <mika@majakorpi.net>
2017-07-31 21:55:29 +0300

    Run npm install for lambda functions and app during deployment

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,12 @@ cacert/gen_ca_cert.sh
 cacert/register_ca_certificate.sh
 cacert/gen_device_cert.sh
 cd button-service
+npm install
 sls deploy
 ./init_iot_logging.sh
 ./setup_app_env.sh
 cd ../app
+npm install
 npm run build
 APP_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | contains("apphostingbucket")) | .Name')
 aws s3 sync build s3://$APP_BUCKET
