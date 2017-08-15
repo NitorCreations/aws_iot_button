5f01af4d1a4e66e14ac1c6cbfde8fe72ab7fb99c
Mika Majakorpi <mika@majakorpi.net>
2017-08-15 16:07:15 +0300

    Fix first time root ca generation

--- cacert/gen_ca_cert.sh
+++ cacert/gen_ca_cert.sh
@@ -5,21 +5,21 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 
 SUBJ="/C=FI/ST=Uusimaa/L=Helsinki/O=Nitor/OU=Nitor/CN=buttonthingCA"
 
-if [ ! -f rootCA.key ] || [ ! -f rootCA.pem ] || [ ! -f rootCA.srl ]; then
+if [ ! -f rootCA.key ] || [ ! -f rootCA.pem ]; then
 
   vault -l rootCA.key -p buttonthing -o rootCA.key
   vault -l rootCA.pem -p buttonthing -o rootCA.pem
-  vault -l rootCA.srl -p buttonthing -o rootCA.srl
+  #vault -l rootCA.srl -p buttonthing -o rootCA.srl
 
 fi
 
-if [ ! -f rootCA.key ] || [ ! -f rootCA.pem ] || [ ! -f rootCA.srl ]; then
+if [ ! -f rootCA.key ] || [ ! -f rootCA.pem ]; then
 
   openssl genrsa -out rootCA.key 2048
   openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 5000 -out rootCA.pem -subj "$SUBJ"
 
   vault -s -w -f rootCA.key -p buttonthing
   vault -s -w -f rootCA.pem -p buttonthing
-  vault -s -w -f rootCA.srl -p buttonthing
+  #vault -s -w -f rootCA.srl -p buttonthing
 
 fi
