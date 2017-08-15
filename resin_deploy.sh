f60ef5d20f5e8ed192d45af1abb7833e13d514c3
Mika Majakorpi <mika@majakorpi.net>
2017-08-15 15:06:26 +0300

    improve resin deploy

--- resin_deploy.sh
+++ resin_deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 cd "$(dirname ${BASH_SOURCE[0]})"
 
-resin app create AWSIoTButton --type raspberrypi3
+if resin app create AWSIoTButton --type raspberrypi3; then fi
 
 resin env add IOT_HOST $(aws iot describe-endpoint | jq -r .endpointAddress) -a AWSIoTButton
 resin env add AWS_ROOT_CA $(cat cacert/awsRootCA.pem | base64) -a AWSIoTButton
@@ -17,12 +17,15 @@ resin env add THING_NAME button -a AWSIoTButton
 
 resin device move 9d748bdcdbc0f560ece3985c189cebcf -a AWSIoTButton
 
-cd ../aws_iot_poc_raspi
+cd raspi
 
 RESIN_USERNAME=$(resin whoami | grep USERNAME | sed 's/USERNAME: //g')
 RESIN_REPO=$(resin app AWSIoTButton | grep 'GIT REPOSITORY' | sed 's/GIT REPOSITORY: //g')
 
-git remote remove resin
+if [ "$(git remote -v | grep -q resin; echo $?)" -eq "0" ]; then
+  git remote remove resin
+fi
+
 git remote add resin $RESIN_USERNAME@git.resin.io:$RESIN_REPO.git
 
 git push -u resin master
